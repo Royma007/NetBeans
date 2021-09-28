5e6e2715c4b9ae7472569c6baa7e1eb3380405d2
Royma007 <36697478+Royma007@users.noreply.github.com>
2021-09-28 16:15:57 +0200

    Add files via upload

---
+++ NetBeansProjects/NumerosAleatorios/src/MetodoAleatorio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dam2
+ */
+/*public class MetodoAleatorio {
+    public static int generaNumAleatorio(int minimo, int maximo){
+            int num=(int)Math.floor(Math.random()*(maximo-minimo+1)+(minimo));
+       return num;
+    }
+}
+*/
